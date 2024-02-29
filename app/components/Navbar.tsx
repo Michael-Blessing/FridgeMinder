b80ef286992976ab4f1174edb7617830917b733c
Irsad03 <irsad.celik@student.htldornbirn.at>
2024-02-29 12:26:16 +0100

    Navbar looks better

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,62 +5,76 @@ import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { UserType } from "../../types/UserType";
+import "./styles/Navbar.css"
+import LogoutIcon from "./styles/images/LogoutIcon";
+
 
 export default function Navbar() {
   const { data: session, update } = useSession();
   const user = session?.user as UserType;
 
   return (
-    <nav className="">
-      <div className="">
-        <a href="/" className="">
-          FridgeMinder
-        </a>
-      </div>
-      <div className="">
+    <nav className="navbar">
+      <div className="logo">
         {user ? (
-          <div className="">
-            <h6 className="">Welcome, {user.name}</h6>
-            <Link
-              style={{ position: "relative", padding: "0 2em 0 0" }}
-              href={{ pathname: `/shoppingCart` }}
-            >
-              <div
-                style={{
-                  position: "absolute",
-                  top: "-10px",
-                  right: "10px",
-                  background: "red",
-                  borderRadius: "50%",
-                  color: "white",
-                  width: "20px",
-                  height: "20px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                {user.cart.length}
-              </div>
-              <FontAwesomeIcon icon={faCartShopping} size="2x" />
-            </Link>
-            <div className="">
-              <Image
-                src={user.image}
-                alt="user profile picture"
-                width={75}
-                height={75}
-              />
-              <a href="/api/auth/signout" className="">
-                Sign Out
-              </a>
-            </div>
-          </div>
-        ) : (
-          <a href="/api/auth/signin" className="">
-            Sign In
+          <a href="/" className="logo">
+            FridgeMinder
           </a>
-        )}
+        ) :
+          <a href="/" className="logo">
+            Sign in for functinons
+          </a>
+        }
+      </div>
+      <div>
+        {user ? (
+          <h6 className="welcome-msg">Welcome, {user.name}</h6>
+        ) : null
+        }
+      </div>
+
+      <div className="profile-and-cart">
+
+        <div className="shoppingcart-and-number">
+          {user ? (
+            <div>
+
+              <Link
+                style={{ position: "relative", padding: "0 2em 0 0" }}
+                href={{ pathname: `/shoppingCart` }}
+              >
+                <div className="shopping-cart">
+                  {user.cart.length}
+                </div>
+
+                <FontAwesomeIcon icon={faCartShopping} size="2x" />
+              </Link>
+            </div>
+          ) : (
+            <a href="/api/auth/signin" className="sign-in-msg">
+              Sign In
+            </a>
+          )}
+        </div>
+        <div className="profile-and-signout">
+          {user ? (<div className="profile-picture">
+            <Image
+              src={user.image}
+              alt="user profile picture"
+              width={75}
+              height={75}
+            />
+          </div>) : null
+          }
+          {user ? (<div>
+
+            <a href="/api/auth/signout" className="sign-out">
+              <LogoutIcon />
+            </a>
+          </div>) : null
+          }
+        </div>
+
       </div>
     </nav>
   );
