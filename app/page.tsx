13af699edcc7574a61c8dc96b0f32b7258ed7133
Noobosaurus <90990447+Noobosaurus@users.noreply.github.com>
2024-02-29 10:49:15 +0100

    Added Cool Neon-Lights to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@
 import ImageUploader from "./components/ImageUploader";
 import { useSession } from "next-auth/react";
 import "../styles/globals.css";
+import "./components/mainpage.css"
 
 export default function Home() {
   const { data: session } = useSession();
 
   return (
-    <>
-      {session ? <ImageUploader /> : <h4 className="">Sign in for features</h4>}
-    </>
+    <div className="main">
+      {session ? <ImageUploader /> : <h4 className="text">Fridgeminder</h4>}
+    </div>
   );
 }
